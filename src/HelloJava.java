8cd74e4bc0f900be5b036b9a294a94ae99980ebd
random-walkie <26143642+random-walkie@users.noreply.github.com>
2024-11-01 11:37:46 +0000

    added HelloComponent class to illustrate a more object-oriented structure for HelloJava application.

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,6 +1,7 @@
-import javax.swing.*;
 //import line at the top is necessary to tell the Java compiler where to find the definitions
 // of the JFrame and JLabel
+import javax.swing.*;
+import java.awt.*;
 
 public class HelloJava {
     public static void main(String[] args) {
@@ -12,10 +13,18 @@ public class HelloJava {
         // we’re going to give HelloJava a graphical user interface (GUI).
         JFrame frame = new JFrame("Hello, Java!"); //This snippet creates a JFrame object with the title
         // “Hello, Java!” JFrame represents a graphical window.
-        JLabel label = new JLabel("Hello, World!", JLabel.CENTER); // JLabel object to display the text centered in our window
-        frame.add(label); // method add , adds JLabel object to frame instance
+        // JLabel label = new JLabel("Hello, World!", JLabel.CENTER); // JLabel object to display the text centered in our window
+        // frame.add(label); // method add , adds label instance to frame instance
+        // Add this line to illustrate an object-oriented structure
+        frame.add(new HelloComponent());
         frame.setSize(300, 150); //  configure its size on the screen using the setSize()
         frame.setVisible(true); // make it visible by calling the setVisible()
     }
 
+}
+
+class HelloComponent extends JComponent {
+    public void paint(Graphics g) {
+        g.drawString("Hello, Java!", 125, 95);
+    }
 }
