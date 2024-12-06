695f1b467d7eaab79dec7984a14dbe5dcf5941a0
random-walkie <26143642+random-walkie@users.noreply.github.com>
2024-12-06 12:20:51 +0000

    added chapter 4 exercises

---
+++ chapter4/src/Euclid.java
@@ -0,0 +1,18 @@
+public class Euclid {
+    public static int euclid(int a, int b) {
+        while (b != 0) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a = 2701;
+        int b = 222;
+        System.out.println("The GCD of " + a + " and " + b + " is " + euclid(a, b));
+    }
+}
---
+++ chapter4/src/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+    public static void triangle(int a){
+        int[][] triangle = new int[a][];
+        for (int i = 0; i < triangle.length; i++) {
+            triangle[i] = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                triangle[i][j] = i + j;
+                System.out.print(triangle[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        triangle(5);
+    }
+}
